309523be4fc44fa8a332c95e0482c22fda4f34a5
Frédéric BARRAS HAMPEL <frederic.hampel@cycloid.io>
2024-03-13 17:27:35 +0100

    misc: add docs on devenv

--- flake.nix
+++ flake.nix
@@ -1,30 +1,33 @@
 {
-  description = "nix devshell example";
+  description = ''
+    Nix developpement shell for this project.
+
+    This will give you a working python 3.11 for executing tests.
+
+    This is not mandatory. it's just help for those using nix.
+  '';
 
   inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
 
   outputs = { self, nixpkgs }:
     let
       allSystems = [
-        "x86_64-linux" # 64bit AMD/Intel x86
+        "x86_64-linux"
       ];
 
       forAllSystems = fn:
         nixpkgs.lib.genAttrs allSystems
         (system: fn { pkgs = import nixpkgs { inherit system; }; });
     in {
-      # nix develop <flake-ref>#<name>
-      # -- 
-      # $ nix develop <flake-ref>#first
       devShells = forAllSystems ({ pkgs }: {
         default = pkgs.mkShell {
           name = "default";
           nativeBuildInputs = with pkgs; [
+            watchexec
             (python311.withPackages (p: with p; [
               ipython
               packaging
               pytest
-              ruff
               docker
               ansible
             ]))
